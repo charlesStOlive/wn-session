6ff94952b6c2389ac2457168c5f164a05552b839
Charles <charles@notilac.fr>
2023-03-24 18:36:24 +0100

    correction lang

--- lang/fr/wakasession.php
+++ lang/fr/wakasession.php
@@ -2,7 +2,7 @@
 return [
     'data_source' => 'Data source',
     'default' => 'Données par défault',
-    'ds_id_test' => 'Data source par défaut',
+    'ds_id_test' => 'Modèle Data source par défaut',
     'embed_all_ds' => 'Embarquer tous les données',
     'embed_all_ds_com' => 'Déconseillé. Utilse si données complexes dans les contenus mais multiplie les requêtes',
     'has_ds' => 'Utilise une data source',
