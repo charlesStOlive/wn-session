be91f33937fecdc9add403d4bd7965ebd698826a
Charles <charles@notilac.fr>
2023-04-07 13:13:20 +0200

    MAJ LANG

---
+++ lang/fr/lang.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'plugin' => 'WakaSession',
+    'userkey' => [
+        '_dataToString' => 'Données (textes)',
+        'create' => 'Créer',
+        'file' => 'Fichiers',
+        'files' => 'Fichier',
+        'form_name' => 'Gestion session',
+        'image' => 'Image ',
+        'images' => 'Images',
+        'key_end_at' => 'Fin de la clef',
+        'name' => 'Clef',
+        'owner' => 'Porteur de la clef',
+        'preview_name' => 'Preview',
+        'secret' => 'Code secret',
+        'slug' => 'Slug',
+        'title' => 'Titre',
+        'update' => 'MAJ',
+        'waka_session' => 'Session'
+    ],
+    'wakasession' => [
+        'create' => 'Créer',
+        'data_source' => 'Data source',
+        'ds_id_test' => 'Modèle Data source par défaut',
+        'embed_all_ds' => 'Embarquer tous les données',
+        'embed_all_ds_com' => 'Déconseillé. Utilse si données complexes dans les contenus mais multiplie les requêtes',
+        'form_name' => 'Session',
+        'has_ds' => 'Utilise une data source',
+        'key_duration' => 'Durée de la clef',
+        'mapping' => 'Mappage',
+        'name' => 'Intitulé de la cléf',
+        'preview_name' => 'Preview',
+        'title' => 'Titre',
+        'update' => 'MAJ',
+        'url' => 'Url de la landing page'
+    ]
+];
